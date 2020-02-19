93cc50dabe90e26ea1f03f1cec54313a2b73af56
Matheus Gama dos Santos <36708181+MatheusGSantos@users.noreply.github.com>
2020-02-19 18:00:45 -0300

    Add files via upload

--- corpus-2/main.py
+++ corpus-2/main.py
@@ -1,18 +1,8 @@
 from collections import Counter
+import re
 import json
 
 
-def check_spcase(string):
-    """
-    Returns true if the string is a digit(single digit or first 2 at least) or a single consonant
-    """
-    if ( (len(string) == 1) and (string not in 'AEIOU') or (string.isdigit() ) or ( string[:2].isdigit() ) ):
-        return True
-    else:
-        return False
-
-
-skippable_chars = ",:; \\()[]{}…+-.º\n" # string containing each character that doesn't 
 c = Counter() # dict like data structure, but presumably faster for this situation
 
 
@@ -28,16 +18,17 @@ with open("corpus-q2.csv", 'r') as input_file:
             else:
                 each_sentence = each_sentence[:ocurrence]+each_sentence[ocurrence+1:] # removes the " "
 
-        for sep in skippable_chars[1:]: # for each separator in skippable_chars string, execept the first one
-            each_sentence = each_sentence.replace(sep,skippable_chars[0])   # replace the current character with the first character (,)
-        list_sentence = [word for word in each_sentence.split(skippable_chars[0]) if word] # each_sentence is almost a list of actual words
+        list_sentence = re.split(r'\W+',each_sentence)
 
         # count word
         for elem in list_sentence:
-            if check_spcase(elem):
-                continue    # ignore it
-            else:              
+            if len(elem) > 1 and not elem[0].isdigit(): # non-digit
                 c[elem.lower()] += 1 # count
+            else:
+                if elem not in ['A','E','O']: # not A, E, O
+                    continue    # ignore it
+                c[elem.lower()] += 1 # count otherwise
 
-with open('words.json', "w", encoding='utf-8') as output:   # write to Json file
+
+with open("words.json", 'w', encoding="utf-8") as output:   # write to Json file
     json.dump(c, output, ensure_ascii=False, indent=4)
